19ab02209793ab86acc7281e2587bee80093e07f
siyongoh <dev@syoh.net>
2021-11-17 03:17:10 +0900

    Add lec k8s02-02-05 init code

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+client = MongoClient("mongodb://localhost:27017/")
+db = client.dbKuber
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/memo', methods=['POST'])
+def save_post():
+    params = request.get_json()
+
+    post = {
+        'title': params['title'],
+        'content': params['content'],
+    }
+    db.memo.insert_one(post)
+    return jsonify({"result": "success"})
+
+
+if __name__ == '__main__':
+    app.run()
